95b637c992cd553f878753ed5ea67b98c30b1d22
rkikkeri13 <61066787+rkikkeri13@users.noreply.github.com>
2025-02-28 14:35:34 +0530

    Update Prac.java

--- Practice/src/Practice/Prac.java
+++ Practice/src/Practice/Prac.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import static org.junit.Assert.assertTrue;
 
 
 public class Prac {
